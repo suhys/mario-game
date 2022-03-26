9c79401cc4428db6d265f85d4b40d7c67194fb6d
suhys <yuseung.suh@gmail.com>
2022-03-25 22:43:19 -0700

    Create game.py, game.data.py, settings.py - basic settings

--- game.py
+++ game.py
@@ -4,4 +4,16 @@ from settings import Settings
 
 class Game:
     def __init__(self):
-    pygame.init()
+        pygame.init()
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode((self.settings.screen_width, 
+                                                self.settings.screen_height))
+        self.bg_color = self.settings.bg_color
+        
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
