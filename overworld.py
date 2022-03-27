47706f01631bbaca4f49a2a760fadf271c9db987
suhys <yuseung.suh@gmail.com>
2022-03-27 16:36:24 -0700

    Update Overworld - Create Player Icon

--- overworld.py
+++ overworld.py
@@ -1,7 +1,6 @@
 import pygame as pg
 from game_data import levels
-from pygame.sprite import Group, Sprite
-
+from pygame.sprite import Group, Sprite, GroupSingle
 
 class Overworld:
     def __init__(self, game):
@@ -15,6 +14,7 @@ class Overworld:
         #sprites
         self.nodes = Group()
         self.setup_nodes()
+        self.setup_icon()
         
     def setup_nodes(self):
         for index, node_data in enumerate(levels.values()):
@@ -23,12 +23,30 @@ class Overworld:
             else:
                 node_sprite = Node(node_data['node_pos'], 'locked')
             self.nodes.add(node_sprite)
+            
+    def draw_paths(self):
+        points = [node['node_pos'] for index,node in enumerate(levels.values()) if index <= self.max_level]
+        pg.draw.lines(self.screen, 'red', False, points, 6)
+        
+    def setup_icon(self):
+        self.icon = GroupSingle()
+        icon_sprite = Icon(self.nodes.sprites()[self.current_level].rect.center)
+        self.icon.add(icon_sprite)
 
     def run(self):
+        self.draw_paths()
         self.nodes.draw(self.screen)
+        self.icon.draw(self.screen)
+        
+class Icon(Sprite):
+    def __init__(self,pos):
+        super().__init__()
+        self.image = pg.Surface((20,20))
+        self.image.fill('blue')
+        self.rect = self.image.get_rect(center = pos)
 
 class Node(Sprite):
-    def __init__(self,pos, status):
+    def __init__(self,pos,status):
         super().__init__()
         self.image = pg.Surface((100, 80))
         if status == 'available':
